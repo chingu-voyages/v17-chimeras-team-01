580eb9c38b1065f99a784db1304fd42486c2a3bb
jmoralesg <johanamgff@gmail.com>
2020-03-24 18:05:01 +1100

    set text and color of badge

--- eventPage.js
+++ eventPage.js
@@ -1,17 +1,16 @@
-// to retrieve all the tabs
-chrome.tabs.query({}, function(tabs) {
-  let session = {
-    name: "",
-    tabsArray: []
-  };
-
-  tabs.forEach(function(tab) {
-    let tabItem = {};
-    if (tab.url.indexOf("chrome-devtools://") !== 0) {
-      tabItem.title = tab.title;
-      tabItem.url = tab.url;
-      tabItem.favIconUrl = tab.favIconUrl;
-      session.tabsArray.push(tabItem);
-    }
+// chrome tab count
+function updateBadge(tabId, changeInfo, tab) {
+  chrome.tabs.query({ currentWindow: true }, function(tabs) {
+    let numberTabs = tabs.length;
+    let numberTabsString = numberTabs.toString();
+    chrome.browserAction.setBadgeText({ text: numberTabsString });
+    chrome.browserAction.setBadgeBackgroundColor({ color: "#ff6347" });
   });
-});
+}
+
+chrome.tabs.onCreated.addListener(updateBadge);
+chrome.tabs.onRemoved.addListener(updateBadge);
+chrome.tabs.onReplaced.addListener(updateBadge);
+chrome.windows.onFocusChanged.addListener(updateBadge);
+
+updateBadge();
