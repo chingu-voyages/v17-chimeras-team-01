482bf2e419df413e5e5ad4036e44225dd4b914bc
Keiko Tamaki <tmk.d.i.kk@gmail.com>
2020-03-31 10:36:14 +0900

    refactored 'options_delete.js' (changed const name)

--- extension/options_delete.js
+++ extension/options_delete.js
@@ -4,11 +4,11 @@ const definedDeleteButton = Array.from(document.getElementsByClassName('delete-b
 definedDeleteButton.map((link, i) => {
   link.addEventListener('click', function (e) {
     // delete one of link tag
-    const removedId = link.parentNode
-    removedId.remove()
+    const targetLink = link.parentNode
+    targetLink.remove()
     chrome.storage.local.get(['stock'], function (data) {
           let stock = data.stock // eslint-disable-line
-      const n = removedId.id
+      const n = targetLink.id
       const changedStock = stock.filter(d => d.id !== n)
       stock = changedStock
       chrome.storage.local.set({ stock })
