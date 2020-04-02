1e7db190512b23e831050b3a37f5fd6d7e563c94
Keiko Tamaki <tmk.d.i.kk@gmail.com>
2020-04-02 15:54:49 +0900

    refactoring (change the structure of html of options)

--- extension/options.js
+++ extension/options.js
@@ -1,54 +1,61 @@
 // for insert nodes
-const outsideLinks = document.getElementById('outside-links')
+const outsideLinksUl = document.getElementById('outside-links-ul')
+
+// get stored data from chrome.storage
+function getStoredData () {
+  chrome.storage.local.get(['stock'], function (data) {
+    // store elements
+    const fragment = document.createDocumentFragment()
+
+    // create blocks for adding links per saved each one
+    const outsideLinksLi = document.createElement('li')
+    outsideLinksLi.className = 'outside-links-li'
+    const insideLinksUl = document.createElement('ul')
+    insideLinksUl.id = 'intside-links-ul'
+
+    // create each link of tabs
+    const allStockedData = Array.from(data.stock)
+    allStockedData.forEach(link => {
+      // create elements for adding links
+      const li = document.createElement('li')
+      li.className = 'saved-link'
+      li.id = link.id
+
+      const icon = document.createElement('img')
+      icon.src = link.src
+      icon.alt = `${link.title}`
+
+      const a = document.createElement('a')
+      a.href = link.url
+      a.target = '_blank'
+
+      const tabLink = document.createTextNode(`${link.title}`)
+
+      const deleteButton = document.createElement('button')
+      const deleteButtonImage = document.createElement('img')
+      deleteButton.className = 'delete-button'
+      deleteButtonImage.src = 'img/clear-black-18dp.svg' // this is a material icon https://material.io/resources/icons
+      deleteButtonImage.alt = 'clear button'
+
+      // append elements
+      a.appendChild(tabLink)
+      deleteButton.appendChild(deleteButtonImage)
+      li.appendChild(icon)
+      li.appendChild(a)
+      li.appendChild(deleteButton)
+
+      // collect elements
+      fragment.appendChild(li)
+    })
+    // append all element
+    insideLinksUl.appendChild(fragment)
+    outsideLinksLi.appendChild(insideLinksUl)
+    outsideLinksUl.appendChild(outsideLinksLi)
+  })
+}
 
 function createBase () {
-  document.addEventListener('DOMContentLoaded', function () {
-    // get stored data from chrome.storage
-    function getStoredData () {
-      chrome.storage.local.get(['stock'], function (data) {
-        // store elements
-        const fragment = document.createDocumentFragment()
-
-        // create each link of tabs
-        const allStockedData = Array.from(data.stock)
-        allStockedData.forEach(link => {
-          // create elements for adding links
-          const li = document.createElement('li')
-          li.className = 'saved-link'
-          li.id = link.id
-
-          const icon = document.createElement('img')
-          icon.src = link.src
-          icon.alt = `${link.title}`
-
-          const a = document.createElement('a')
-          a.href = link.url
-          a.target = '_blank'
-
-          const tabLink = document.createTextNode(`${link.title}`)
-
-          const deleteButton = document.createElement('button')
-          const deleteButtonImage = document.createElement('img')
-          deleteButton.className = 'delete-button'
-          deleteButtonImage.src = 'img/clear-black-18dp.svg' // this is a material icon https://material.io/resources/icons
-          deleteButtonImage.alt = 'clear button'
-
-          // append elements
-          a.appendChild(tabLink)
-          deleteButton.appendChild(deleteButtonImage)
-          li.appendChild(icon)
-          li.appendChild(a)
-          li.appendChild(deleteButton)
-
-          // collect elements
-          fragment.appendChild(li)
-        })
-        // append all element
-        outsideLinks.appendChild(fragment)
-      })
-    }
-    getStoredData()
-  }, false)
+  document.addEventListener('DOMContentLoaded', getStoredData, false)
 }
 
 // document.addEventListener('DOMContentLoaded', function () {
@@ -77,9 +84,9 @@ function deleteLinks () {
       // if (definedDeleteButtons.length) {
       // for (let i = definedDeleteButtons.length; i >= 0; i--) {
       if (e.target) {
-        if (outsideLinks.children.length === 0) {
+        if (outsideLinksUl.children.length === 0) {
           const noLink = document.createTextNode('there is no link')
-          outsideLinks.appendChild(noLink)
+          outsideLinksUl.appendChild(noLink)
         } else { console.log('something happening') }
       }
       //   break
