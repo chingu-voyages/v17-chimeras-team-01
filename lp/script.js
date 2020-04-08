c2bec4cbafca5a7dc47d0794a2e7f185f3d43dcc
Keiko Tamaki <tmk.d.i.kk@gmail.com>
2020-04-08 19:44:05 +0900

    adjust compressed tabs for adjusting width

--- lp/script.js
+++ lp/script.js
@@ -1,7 +1,9 @@
+// --------------------
 // for top tab illust
+// --------------------
 const TabIllust = document.querySelector('.upper__inner')
 
-// browser buttons
+// base browser buttons
 const BrowserButtons = document.createElement('div')
 BrowserButtons.className = 'browser-buttons'
 TabIllust.appendChild(BrowserButtons)
@@ -10,15 +12,14 @@ const BrowserButtonColor = [
   'red', 'yellow', 'green'
 ]
 
+// create browser buttons
 function createButton (color) {
   const BrowserButton = document.createElement('div')
   BrowserButton.className = `browser-button browser-button-${color}`
   return BrowserButton
-  // return TabIllust.appendChild(BrowserButton) たぶんちがう
-  // return `
-  //   <div class="browser-button browser-button-${color}"></div>`
 }
 
+// add browser buttons
 function addButtons () {
   const fragment = document.createDocumentFragment()
   BrowserButtonColor.map((color) => {
@@ -26,36 +27,38 @@ function addButtons () {
     fragment.appendChild(BrowserButtons)
   })
   return BrowserButtons.appendChild(fragment)
-
-  // TabIllust.innerHTML = BrowserButtonColor.map((color) => {
-  //   return createButton(color)
-  // })
-  //   .join('')
 }
 addButtons()
 
-// bunch of tabs
+// base compressed tabs
 const TabWrapper = document.createElement('div')
 TabWrapper.className = 'compressed-tabs'
 TabIllust.appendChild(TabWrapper)
 
+// create one of compressed tabs
 function createCompressedTabs () {
   const CompressedTab = document.createElement('div')
   CompressedTab.className = 'compressed-tab'
   return CompressedTab
-  // return `
-  //       <div class="compressed-tab"></div>`
 }
 
-// function createtTabs (tab) {
-//   return `
-//       <div class="compressed-tabs">${tab}</div>`
-// }
-
+// add compressed tabs
 function addTabs () {
+  // change compressed tabs number when resizing
+  if (TabWrapper.firstChild) {
+    while (TabWrapper.firstChild) {
+      TabWrapper.removeChild(TabWrapper.firstChild)
+    }
+  }
   const fragment = document.createDocumentFragment()
   const DivWidth = TabIllust.clientWidth
-  const n = window.innerWidth > 600 ? Math.floor((DivWidth - 200 - 210) / 19) : Math.floor((DivWidth - 106 - 100) / 13)
+
+  let n = 0;  // eslint-disable-line
+  if (window.innerWidth > 600) {
+    n = Math.floor((DivWidth - 200 - 210) / 19)
+  } else {
+    n = Math.floor((DivWidth - 106 - 100) / 13)
+  }
 
   for (let i = 0; i < n; i++) {
     const CompressedTabs = createCompressedTabs()
@@ -63,24 +66,26 @@ function addTabs () {
   }
 
   return TabWrapper.appendChild(fragment)
-  // TabIllust.innerHTML = createtTabs(createCompressedTab())
 }
 addTabs()
+
+window.addEventListener('resize', addTabs, false)
+
+// a set of add and close functions
+const AddCloseBlock = document.createElement('div')
+AddCloseBlock.className = 'add-close-block'
+TabIllust.appendChild(AddCloseBlock)
 
 // common cross mark
 const CommonCrossMark = document.createElement('div')
 CommonCrossMark.className = 'common-cross-mark rotate'
 
-// add-close-block
-const AddCloseBlock = document.createElement('div')
-AddCloseBlock.className = 'add-close-block'
-TabIllust.appendChild(AddCloseBlock)
-
-// single tab
+// single tab -- outside
 const SingleTab = document.createElement('div')
 SingleTab.className = 'single-tab'
 AddCloseBlock.appendChild(SingleTab)
 
+// single tab -- inside
 const SingleTabDeco = document.createElement('span')
 SingleTabDeco.className = 'single-tab-deco'
 SingleTab.appendChild(SingleTabDeco)
